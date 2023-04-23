921ef0eea99d43ff2a56041af2e50a74c1611abc
Aziz Ullah Khan <bc210413454skh@vu.edu.pk>
2023-04-23 22:37:22 +0500

    app updates

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome from Docker! This is updated file!!' 
 
+@app.route('/app')
+def hello_world():
+    return 'This is from APP.......' 
+
  
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
